5fb67c69056d8c7365c2a9fcbc0795b8e8b707fc
mushroom <syafiqahalim.work@gmail.com>
2022-04-04 03:03:28 +0000

    edit

--- resources/views/admin/rooms/create.blade.php
+++ resources/views/admin/rooms/create.blade.php
@@ -32,44 +32,6 @@
                     <span class="help-block">{{ trans('cruds.room.fields.name_helper') }}</span>
                 </div>
 
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="levels">{{ trans('cruds.room.fields.levels') }}</label>
-                        <div style="padding-bottom: 4px">
-                            <span class="btn btn-info btn-xs select-all" style="border-radius: 0">{{ trans('global.select_all') }}</span>
-                            <span class="btn btn-info btn-xs deselect-all" style="border-radius: 0">{{ trans('global.deselect_all') }}</span>
-                        </div>
-                        <select class="form-control select2 {{ $errors->has('levels') ? 'is-invalid' : '' }}" name="levels[]" id="levels" multiple>
-                            @foreach($levels as $id => $level)
-                                <option value="{{ $id }}" {{ in_array($id, old('levels', [])) ? 'selected' : '' }}>{{ $level }}</option>
-                            @endforeach
-                        </select>
-                        @if($errors->has('levels'))
-                            <div class="invalid-feedback">
-                                {{ $errors->first('levels') }}
-                            </div>
-                        @endif
-                        <span class="help-block">{{ trans('cruds.room.fields.levels_helper') }}</span>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="sections">{{ trans('cruds.room.fields.section') }}</label>
-                        <div style="padding-bottom: 4px">
-                            <span class="btn btn-info btn-xs select-all" style="border-radius: 0">{{ trans('global.select_all') }}</span>
-                            <span class="btn btn-info btn-xs deselect-all" style="border-radius: 0">{{ trans('global.deselect_all') }}</span>
-                        </div>
-                        <select class="form-control select2 {{ $errors->has('sections') ? 'is-invalid' : '' }}" name="sections[]" id="sections" multiple>
-                            @foreach($sections as $id => $section)
-                                <option value="{{ $id }}" {{ in_array($id, old('sections', [])) ? 'selected' : '' }}>{{ $section }}</option>
-                            @endforeach
-                        </select>
-                        @if($errors->has('sections'))
-                            <div class="invalid-feedback">
-                                {{ $errors->first('sections') }}
-                            </div>
-                        @endif
-                        <span class="help-block">{{ trans('cruds.room.fields.section_helper') }}</span>
-                    </div>
-                </div>
                 <div class="form-group">
                     <a class="btn btn-default" href="{{ route('admin.rooms.index') }}">
                         {{ trans('global.back_to_list') }}
