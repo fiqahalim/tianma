3c94b71aef032a5715c732eb215ef358ac5507a6
mushroom <syafiqahalim.work@gmail.com>
2022-05-18 01:42:04 +0000

    modified invoice receipt

--- resources/views/pages/product/booking-lot.blade.php
+++ resources/views/pages/product/booking-lot.blade.php
@@ -13,24 +13,12 @@
     <div class="container-fluid">
         <form method="POST" action="{{ route('admin.product-booking.store', [$product->categories->first()->parentCategory->name, $product->categories->first()->parentCategory->name, $product->categories->first()->name, $product]) }}" enctype="multipart/form-data" id="bookingForm">
             @csrf
+            <input type="text" name="price" hidden="">
 
             <img src="{{ $product->photo->url ?? '/images/product/luxury_1.png' }}" class="rounded mx-auto d-block" style="height: 300px; width: 485px;">
             <div class="movie-container">
                 <h5><strong>Category Selected:</strong> {{ $product->product_name }}
                 </h5>
-                {{-- <select class="form-control {{ $errors->has('section') ? 'is-invalid' : '' }}" name="section" id="section" required>
-                    <option selected>{{ trans('global.pleaseSelect') }} section</option>
-                    <option value="DA">DA</option>
-                    <option value="DB">DB</option>
-                    <option value="DC">DC</option>
-                    <option value="DD">DD</option>
-                </select>
-                @if($errors->has('section'))
-                    <div class="invalid-feedback">
-                        {{ $errors->first('section') }}
-                    </div>
-                @endif
-                <span class="help-block">{{ trans('cruds.order.fields.order_status_helper') }}</span> --}}
             </div>
             <ul class="showcase">
                 <li>
@@ -47,67 +35,67 @@
                 </li>
             </ul>
 
-            <div class="row gx-xl-5 gy-4 gy-sm-5 justify-content-center">
+            <div class="row">
                 <div class="col-lg-4 col-md-6">
                     <div class="seat-overview-wrapper">
-                        <form action="" method="POST" id="bookingForm" class="row gy-2">
-                            @csrf
-                            <input type="text" name="" hidden="">
-                            <div class="col-12">
-                                <div class="form-group">
-                                    <label for="rooms" class="form-label">Room</label>
-                                    <select class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }} mb-3" name="rooms" required id="rooms">
-                                        @forelse($rooms as $r => $room)
-                                            <option value="{{ $room }}" {{ old('name', '') === (string) $room ? 'selected' : '' }}>
-                                                {{ $room }}
-                                            </option>
-                                        @empty
-                                            <option>No rooms founds</option>
-                                        @endforelse
-                                    </select>
-                                </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label for="rooms" class="form-label font-weight-bold">Room</label>
+                                <select class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }} mb-3" name="rooms" required id="rooms">
+                                    @forelse($rooms as $r => $room)
+                                        <option value="{{ $room }}" {{ old('name', '') === (string) $room ? 'selected' : '' }}>
+                                            {{ $room }}
+                                        </option>
+                                    @empty
+                                        <option>No rooms founds</option>
+                                    @endforelse
+                                </select>
                             </div>
-                            <div class="col-12">
-                                <div class="form-group">
-                                    <label for="section" class="form-label">Section</label>
-                                    <select class="form-control {{ $errors->has('sections') ? 'is-invalid' : '' }}" name="sections" id="sections" required>
-                                        @forelse($sections as $sc => $section)
-                                            <option value="{{ $sc }}" {{ old('section', '') === (string) $sc ? 'selected' : '' }}>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label for="section" class="form-label font-weight-bold">Section</label>
+                                <select class="form-control {{ $errors->has('sections') ? 'is-invalid' : '' }}" name="sections" id="sections" required>
+                                    @forelse($sections as $sc => $section)
+                                        <option value="{{ $sc }}" {{ old('section', '') === (string) $sc ? 'selected' : '' }}>
                                                 {{ $section }}
-                                            </option>
-                                        @empty
-                                            <option>No section founds</option>
-                                        @endforelse
-                                    </select>
-                                    @if($errors->has('sections'))
-                                        <div class="invalid-feedback">
-                                            {{ $errors->first('section') }}
-                                        </div>
-                                    @endif
-                                    <span class="help-block">{{ trans('cruds.order.fields.order_status_helper') }}</span>
-                                </div>
+                                        </option>
+                                    @empty
+                                        <option>No section founds</option>
+                                    @endforelse
+                                </select>
+                                @if($errors->has('sections'))
+                                    <div class="invalid-feedback">
+                                        {{ $errors->first('section') }}
+                                    </div>
+                                @endif
+                                <span class="help-block">{{ trans('cruds.order.fields.order_status_helper') }}</span>
                             </div>
-
-                            <div class="booked-seat-details my-3 d-none">
-                                <label>Selected Lots</label>
-                                <div class="list-group seat-details-animate">
-                                    <span class="list-group-item d-flex bg--base text-white justify-content-between">
-                                        Lots Details
-                                    </span>
-                                    <div class="selected-seat-details"></div>
-                                </div>
-                            </div>
-                            <input type="text" name="seats" hidden="">
-                            <div class="col-12">
-                                <input type="text" name="seat" hidden>
-                                <button type="submit" class="btn btn-outline-dark mt-2 book-bus-btn" onclick="updateTextArea()">
-                                    {{ trans('global.products.product_select') }}
-                                </button>
-                            </div>
-                        </form>
+                        </div>
+                        <div class="d-grid gap-2 col-6 mx-auto">
+                            <button type="submit" class="btn btn-outline-dark mt-2 book-bus-btn btn-block">
+                                {{ trans('global.products.product_select') }}
+                            </button>
+                        </div>
                     </div>
                 </div>
+                <div class="col-lg-8 col-md-6">
+                    <div class="seat-overview-wrapper">
+                        <div class="booked-seat-details d-none">
+                            <label class="form-label font-weight-bold">Selected Lots</label>
+                            <div class="list-group seat-details-animate">
+                                <span class="list-group-item d-flex bg--base text-white justify-content-between">
+                                    Lots Details
+                                </span>
+                                <div class="selected-seat-details"></div>
+                            </div>
+                        </div>
+                        <input type="text" name="seats" hidden="">
+                    </div>
+                </div>
+            </div>
 
+            <div class="row mt-5">
                 <div class="col-lg-4 col-md-6">
                     <h6 class="title">@lang('Click on Lot to select or deselect')</h6>
                     @foreach ($trip->bookingSection as $seat)
@@ -122,7 +110,7 @@
                                     $lastRowSeat = $lotLayout->getLastRowSit($seat);
                                     $chr = 'A';
                                     $deckIndex = $loop->index + 1;
-                                    $seatlayout = $lotLayout->sitLayouts();
+                                    $seatlayout = $lotLayout->lotLayouts();
                                     $rowItem = $seatlayout->left + $seatlayout->right;
                                 @endphp
 
@@ -132,7 +120,7 @@
                                         break;
                                         $seatNumber = $chr;
                                         $chr++;
-                                        $seats = $lotLayout->getSeats($deckIndex,$seatNumber);
+                                        $seats = $lotLayout->getLots($deckIndex,$seatNumber);
                                     @endphp
 
                                     <div class="seat-wrapper">
@@ -144,7 +132,7 @@
                                 @if($lastRowSeat == 1)
                                     @php $seatNumber++ @endphp
                                     <div class="seat-wrapper justify-content-center">
-                                        @for ($lsr=1; $lsr <= $rowItem+1; $lsr++) @php echo $busLayout->generateSeats($lsr,$deckIndex,$seatNumber); @endphp
+                                        @for ($lsr=1; $lsr <= $rowItem+1; $lsr++) @php echo $lotLayout->generateLots($lsr,$deckIndex,$seatNumber); @endphp
                                             @endfor
                                     </div>
                                 @endif
@@ -152,8 +140,8 @@
                                 @if($lastRowSeat > 1)
                                     @php $seatNumber++ @endphp
                                     <div class="seat-wrapper justify-content-center">
-                                        @for($l = 1; $l <= $lastRowSeat; $l++) @php echo $busLayout->generateSeats($l,$deckIndex,$seatNumber); @endphp
-                                            @endfor
+                                        @for($l = 1; $l <= $lastRowSeat; $l++) @php echo $lotLayout->generateLots($l,$deckIndex,$seatNumber); @endphp
+                                        @endfor
                                     </div>
                                 @endif
                             </div>
@@ -193,87 +181,4 @@
 @section('scripts')
     @parent
     <script type="text/javascript" src="{{ mix('/js/pages/booking.js') }}"></script>
-    <script>
-        $(document).ready(function() {
-            $("select").on('change', function() {
-                $(this).find("option:selected").each(function() {
-                    var geeks = $(this).attr("value");
-                    if (geeks) {
-                        $(".GFG").not("." + geeks).hide();
-                        $("." + geeks).show();
-                    } else {
-                        $(".GFG").hide();
-                    }
-                });
-            }).change();
-        });
-
-        //reset all lots
-        function reset() {
-            $('.seat-wrapper .seat').removeClass('selected');
-            $('.selected-seat-details').html('');
-        }
-
-        //click on lot
-        $('.seat-wrapper .seat').on('click', function() {
-            var rooms = $('select[name="rooms"]').val();
-            var sections = $('select[name="sections"]').val();
-
-            if (rooms && sections) {
-                selectLot();
-            } else {
-                $(this).removeClass('selected');
-                notify('error', "@lang('Please select room and section before select any seat')")
-            }
-        });
-
-        //select and booked seat
-        function selectLot() {
-            let selectedSeats = $('.seat.selected');
-            let seatDetails = ``;
-            let price = $('input[name=price]').val();
-            let subtotal = 0;
-            let currency = '{{ __($general->cur_text) }}';
-            let seats = '';
-            if (selectedSeats.length > 0) {
-                $('.booked-seat-details').removeClass('d-none');
-                $.each(selectedSeats, function(i, value) {
-                    seats += $(value).data('seat') + ',';
-                    seatDetails += `<span class="list-group-item d-flex justify-content-between">${$(value).data('seat')} <span>${price} ${currency}</span></span>`;
-                    subtotal = subtotal + parseFloat(price);
-                });
-
-                $('input[name=seats]').val(seats);
-                $('.selected-seat-details').html(seatDetails);
-                $('.selected-seat-details').append(`<span class="list-group-item d-flex justify-content-between">@lang('Sub total')<span>${subtotal} ${currency}</span></span>`);
-            } else {
-                $('.selected-seat-details').html('');
-                $('.booked-seat-details').addClass('d-none');
-            }
-        }
-
-        //on change rooms and sections show available lots
-        $(document).on('change', 'select[name="rooms"], select[name="sections"]', function(e) {
-            showBookedSeat();
-        });
-
-        //booking form submit
-        $('#bookingForm').on('submit', function(e) {
-            e.preventDefault();
-            let selectedSeats = $('.seat.selected');
-            if (selectedSeats.length > 0) {
-                var modal = $('#bookConfirm');
-                modal.modal('show');
-            } else {
-                notify('error', 'Select at least one lot.');
-            }
-        });
-
-        //confirmation modal
-        $(document).on('click', '#btnBookConfirm', function(e) {
-            var modal = $('#bookConfirm');
-            modal.modal('hide');
-            document.getElementById("bookingForm").submit();
-        });
-    </script>
 @endsection
